2a0c269c49ae4e391200f9ee2604a04780ad73fa
bsparx <m.javed.24463@khi.iba.edu.pk>
2026-02-10 00:40:33 +0500

    lol

---
+++ app/new-user/page.tsx
@@ -0,0 +1,29 @@
+import prisma from "@/utils/db";
+import { currentUser } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+
+export default async function connect() {
+    const user = await currentUser();
+
+    if (!user || !user.username) {
+        return redirect("/sign-in");
+    }
+
+    const match = await prisma.user.upsert({
+        where: {
+            clerkId: user.id as string,
+        },
+        update: {
+            email: user.emailAddresses[0].emailAddress,
+            name: user.username,
+        },
+        create: {
+            clerkId: user.id,
+            email: user.emailAddresses[0].emailAddress,
+            name: user.username,
+        },
+    });
+
+
+    redirect("/register");
+}
